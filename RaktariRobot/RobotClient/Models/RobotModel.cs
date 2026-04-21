c0c9020f285b6c00388340bcffbf677fa198b71f
Akos Porkolab <porkolab.akosbenjamin@edu.bme.hu>
2026-04-21 20:58:23 +0200

    3 model and 3 view for 10 points (robot, logs, settings)

---
+++ RaktariRobot/RobotClient/Models/RobotModel.cs
@@ -0,0 +1,18 @@
+﻿using RobotShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuntimeTerror.RobotClient.Models
+{
+    public class RobotModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public double X { get; set; }
+        public double Y { get; set; }
+        public int Battery { get; set; }
+        public RobotState State { get; set; } = RobotState.Ready;
+    }
+}
