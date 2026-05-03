badd4068f2d3bd4e9029e4460193005cb5fb9d1c
levente-abraham3 <levente.abraham3@gmail.com>
2026-05-03 21:07:17 +0200

    Enums.cs fájl létrehozása a RobotShared projektben a markdown leírás alapján

---
+++ RaktariRobot/RobotShared/Enums.cs
@@ -0,0 +1,30 @@
+﻿namespace RobotShared
+{
+    public enum RobotState
+    {
+        Idle,
+        MovingToShelf,
+        MovingToDropoff,
+        MovingToCharger,
+        MovingToService,
+        ManualMoving,
+        Charging,
+        Paused,
+        Error
+    }
+
+    public enum DiagnosticLevel
+    {
+        Normal,
+        Warning,
+        CriticalWarning,
+        Error
+    }
+
+    public enum ComponentStatus
+    {
+        Normal,
+        Warning,
+        Error
+    }
+}
