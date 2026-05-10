79eea6f501ddeeb28408017bc13b9d32d365143f
levente-abraham3 <levente.abraham3@gmail.com>
2026-05-10 15:36:42 +0200

    WarehouseController.cs és WarehouseService commit megint

--- RaktariRobot/RobotServer/Controllers/WarehouseController.cs
+++ RaktariRobot/RobotServer/Controllers/WarehouseController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using RobotServer.Services;
 using RobotShared;
-using System.Collections.Generic;
 
 namespace RobotServer.Controllers
 {
@@ -8,27 +8,17 @@ namespace RobotServer.Controllers
     [Route("api/warehouse")]
     public class WarehouseController : ControllerBase
     {
+        private readonly WarehouseService _warehouseService;
+
+        public WarehouseController(WarehouseService warehouseService)
+        {
+            _warehouseService = warehouseService;
+        }
+
         [HttpGet]
         public ActionResult<WarehouseDto> GetWarehouse()
         {
-            // Dummy warehouse adatok (10x7).
-            var warehouse = new WarehouseDto
-            {
-                Width = 10,
-                Height = 7,
-                SpawnPosition = new PositionDto { X = 7, Y = 2 }, // 'R' pozíció
-                ChargerPosition = new PositionDto { X = 9, Y = 3 }, // 'C'
-                DropoffPosition = new PositionDto { X = 8, Y = 6 }, // 'D'
-                ServicePosition = new PositionDto { X = 2, Y = 0 }, // 'W'
-                Shelves = new List<ShelfDto>
-                {
-                    new ShelfDto { ShelfId = "S1", Position = new PositionDto { X = 7, Y = 0 } },
-                    new ShelfDto { ShelfId = "S2", Position = new PositionDto { X = 3, Y = 1 } },
-                    new ShelfDto { ShelfId = "S3", Position = new PositionDto { X = 0, Y = 2 } },
-                    new ShelfDto { ShelfId = "S4", Position = new PositionDto { X = 4, Y = 4 } },
-                    new ShelfDto { ShelfId = "S5", Position = new PositionDto { X = 0, Y = 5 } }
-                }
-            };
+            var warehouse = _warehouseService.GetWarehouse();
 
             return Ok(warehouse);
         }
--- RaktariRobot/RobotServer/Services/WarehouseService.cs
+++ RaktariRobot/RobotServer/Services/WarehouseService.cs
@@ -1,6 +1,30 @@
-﻿namespace RobotServer.Services
+﻿using RobotShared;
+
+namespace RobotServer.Services
 {
     public class WarehouseService
     {
+        public WarehouseDto GetWarehouse()
+        {
+            return new WarehouseDto
+            {
+                Width = 10,
+                Height = 7,
+
+                SpawnPosition = new PositionDto { X = 7, Y = 2 },
+                ChargerPosition = new PositionDto { X = 9, Y = 3 },
+                DropoffPosition = new PositionDto { X = 8, Y = 6 },
+                ServicePosition = new PositionDto { X = 2, Y = 0 },
+
+                Shelves = new List<ShelfDto>
+                {
+                    new ShelfDto { ShelfId = "S1", Position = new PositionDto { X = 7, Y = 0 } },
+                    new ShelfDto { ShelfId = "S2", Position = new PositionDto { X = 3, Y = 1 } },
+                    new ShelfDto { ShelfId = "S3", Position = new PositionDto { X = 0, Y = 2 } },
+                    new ShelfDto { ShelfId = "S4", Position = new PositionDto { X = 4, Y = 4 } },
+                    new ShelfDto { ShelfId = "S5", Position = new PositionDto { X = 0, Y = 5 } }
+                }
+            };
+        }
     }
 }
